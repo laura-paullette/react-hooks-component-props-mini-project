f883730e346f44de09d23fe1f16b51ad63320460
laura-paullette <laura.paullette@student.moringaschool.com>
2023-04-19 22:56:19 +0300

    completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Article = ({ title, date = "January 1, 1970", preview, minutes }) => {
+   return (
+      <article>
+         <h3>{title}</h3>
+         <small>
+            {date} • {minutes} min read{" "}
+         </small>
+         <p>{preview}</p>
+      </article>
+   );
+};
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./Article";
+
+const ArticleList = ({ posts }) => {
+   return (
+      <main>
+         {posts.map(({ id, title, date, preview, minutes }) => (
+            <Article key={id} title={title} date={date} preview={preview} minutes={minutes} />
+         ))}
+      </main>
+   );
+};
+
+export default ArticleList;
